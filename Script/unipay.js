cf846c5f9182622d81ec0e5cae8ddd233560a01d
jminging <jminging88@gmail.com>
2021-02-18 17:07:17 +0800

    unipay

--- Script/unipay.js
+++ Script/unipay.js
@@ -10,10 +10,10 @@
 var Authorization = '';//手动获取authorization填写此处
 var $tool = tool();
 try {
-	console.log("🍎云闪付签到脚本开始!");
 	var img = "https://is5-ssl.mzstatic.com/image/thumb/Purple114/v4/53/bc/b5/53bcb52a-6c33-67cc-0c70-faf4ffbdb71e/AppIcon-0-0-1x_U007emarketing-0-0-0-6-0-0-85-220.png/230x0w.png";
 	if (typeof $request != "undefined") {
 		if ($request.url.indexOf("youhui.95516.com/newsign/api/coin_details") > -1) {
+			console.log("云闪付获取Cookie")
 			var Cookie = $request.headers["Authorization"];
 			if (!!Cookie) {
 				$tool.setkeyval(Cookie, "authorization");
@@ -61,8 +61,8 @@ try {
 				$tool.notify("云闪付签到失败!", d, d, {img: img});
 			}
 		})
-		$done();
 	}
+	$done();
 } catch (e) {
 	console.log("🍎erro" + e);
 	$tool.notify("云闪付签到错误!", e, e, {img: img});
