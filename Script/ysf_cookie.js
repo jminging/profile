7317d1291a43078e7ae5a1a77901020e29a6a22b
jminging <jminging88@gmail.com>
2023-10-09 08:55:19 +0800

    111

--- Script/ysf_cookie.js
+++ Script/ysf_cookie.js
@@ -22,7 +22,7 @@ function updateToken() {
 	const req = $request;
 	console.log(req.method)
 	console.log(req.headers)
-	if ( req.method == 'GET' && req.headers ) {
+	if ( req && req.method == 'GET' && req.headers ) {
 		token = (req.headers['Authorization'] || req.headers['authorization'] || '')
 		if ( token && updateToken() ) {
 			$.msg($.name + '获取Token成功')
