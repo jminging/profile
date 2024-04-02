d8c1d2bc98884a211207821f641e67decb0f8a7e
jminging <jminging88@gmail.com>
2024-04-02 13:00:42 +0800

    Update meituanwm_cookie.js

--- Script/meituanwm_cookie.js
+++ Script/meituanwm_cookie.js
@@ -17,8 +17,8 @@ const $ = new API(cookieName);
         $.notify(`${cookieName}获取Token成功✅`)
     }
 })().catch(e => {
-	$.msg($.name, '脚本异常，请检查。')
 	console.log(e)
+	$.msg($.name, '脚本异常，请检查。')
 }).finally(_ => {
 	$.done({})
 })
